4c928f0e79a738275c9024bc50644343acf85f76
Matías JC <68836912+DonMotha@users.noreply.github.com>
2025-09-03 19:14:37 +0000

    terminado

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("red").textContent = "Adios";
+
+document.getElementById("orange").style.color = "orange";
+
+const clicTitulo = document.getElementById("click");
+clicTitulo.addEventListener("click", function () {
+  clicTitulo.style.color = "brown";
+});
